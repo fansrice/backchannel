bcba9b76a30fcc1886ab9c49586513e3854794e4
fansrice <yfan5@ncsu.edu>
2014-02-11 21:20:37 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BackchannelV1::Application.config.secret_key_base = '58741db01ee45d341b1449d63d11ad1eb223c7138293b0f379471ccee84f201e6fc3d16e17587e890cc8256cbc28054a8aafeb356b6d49dae62f4212002077de'
